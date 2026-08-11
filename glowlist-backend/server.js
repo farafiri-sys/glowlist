2e0cef363ecc3914c11b43aca8ab46fdbb8abaa2
farafiri-sys <firifara44@gmail.com>
2026-08-11 11:30:27 +0700

    menambahkan produk dan kategori

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database GlowList');
+    }
+});
 const PORT = 3001;
 
 app.use(express.json());
@@ -8,6 +24,22 @@ app.get('/', (req, res) => {
     res.send('Selamat Datang di GlowList API 💄');
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server GlowList jalan di http://localhost:${PORT}`);
-})
+});
+
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
