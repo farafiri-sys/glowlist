e1f9ce73770e332803ff1e7eb8f2059be6a16c1b
farafiri-sys <firifara44@gmail.com>
2026-08-11 15:10:31 +0700

    menambahkan bootstrap ke dalam project dan membuat layout

---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,4 @@
+export default function Tentang() {
+    return <h2>HELLO GUYS! Kenalin, aku Zulfarani dari 12 RPL 2. 
+        Alasanku suka skincare karena selain bikin kulit sehat dia bisa menjaga kulitku biar tetap lembab. </h2>;
+}
